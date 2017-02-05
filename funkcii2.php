cc87b51cbef371c7e4c33c680e471c4649d1baec
vastus676 <vassil767@abv.bg>
2017-02-05 15:21:05 +0200

    a
    a

---
+++ funkcii2.php
@@ -0,0 +1,21 @@
+<?php
+
+//functions HW 
+//task 1
+
+
+// function print_arr($arr,$m,$n){
+// 	for ($i=0; $i < $m ; $i++) { 
+// 		for ($j=0; $j < $n; $j++) { 
+// 			echo "a[". $i . "][" . $j . "] = ". $arr[$i][$j]. ' ';
+// 		}
+// 		echo "<br>";
+// 	}
+// }
+
+// $myArr =[[5,14,2],[20,9,4]];
+
+// print_arr($myArr, 2, 3);
+
+//task 3
+
